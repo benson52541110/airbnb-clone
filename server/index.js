15ace1cfb933c12f87f2b45cdb552ed48d5f20eb
吳冠智 <>
2024-02-07 22:01:07 +0800

    cors url

--- server/index.js
+++ server/index.js
@@ -26,8 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
 	cors({
 		credentials: true,
-		origin:
-			"https://airbnb-clone-client-gcw3jpfy8-benson52541110s-projects.vercel.app",
+		origin: "https://airbnb-clone-client-zeta.vercel.app/",
 	})
 );
 
