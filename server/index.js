93b77bd436506e88d6f3060ef24bcf96de78ecae
吳冠智 <>
2024-02-07 23:52:23 +0800

    test

--- server/index.js
+++ server/index.js
@@ -116,18 +116,17 @@ app.post("/api/login", async (req, res) => {
 							res
 								.cookie("token", token, {
 									httpOnly: true,
-									sameSite: "None",
-									secure: true,
 									domain:
 										process.env.NODE_ENV === "development"
 											? "localhost"
 											: "airbnb-clone-client-zeta.vercel.app",
+									path: "/",
 								})
 								.json(userDoc);
 
 							res.setHeader(
 								"Set-Cookie",
-								`token=${token}; HttpOnly; SameSite=None; Secure; Domain=${
+								`token=${token}; HttpOnly; Domain=${
 									process.env.NODE_ENV === "development"
 										? "localhost"
 										: "airbnb-clone-client-zeta.vercel.app"
