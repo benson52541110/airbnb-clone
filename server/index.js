2c95ec1486d6245c74c4f32c79caccf6578d65c7
吳冠智 <>
2024-02-07 22:04:43 +0800

    no message

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
 	cors({
 		credentials: true,
-		origin: "https://airbnb-clone-client-zeta.vercel.app/",
+		origin: "https://airbnb-clone-client-zeta.vercel.app",
 	})
 );
 
